a4629c99818811e78694d4e5e9b0ef796cb8793c
kannan <kannan@yahoo.com>
2021-09-21 09:16:50 +0800

    second version

--- client/src/App.js
+++ client/src/App.js
@@ -5,77 +5,94 @@ import axios from 'axios';
 
 function App() {
 
-  const [ name, setName] = useState("");
+  const [name, setName] = useState("");
 
-  const [ address, setAddress] = useState("");
-  const [ product, setProduct] = useState("");
-  const [ price, setPrice] = useState(0);
-  const [ quantity, setQuantity] = useState(0);
+  const [address, setAddress] = useState("");
+  const [product, setProduct] = useState("");
+  const [price, setPrice] = useState(0);
+  const [quantity, setQuantity] = useState(0);
 
   const [orderList, setOrderList] = useState([]);
 
   const displayInfo = () => {
-    console.log( name + address + product + price + quantity );
+    console.log(name + address + product + price + quantity);
   }
 
   const addOrder = () => {
-    Axios.post('http://localhost:3001/api/createOrder', { 
-      name:name, 
-      address:address, 
-      product:product, 
-      price:price,
-      quantity:quantity
+    Axios.post('http://localhost:3001/api/createOrder', {
+      name: name,
+      address: address,
+      product: product,
+      price: price,
+      quantity: quantity
     }).then(() => {
       // console.log( "success");
 
-      setOrderList([...orderList, { 
-        name:name, 
-        address:address, 
-        product:product, 
-        price:price,
-        quantity:quantity
+      setOrderList([...orderList, {
+        name: name,
+        address: address,
+        product: product,
+        price: price,
+        quantity: quantity
       }])
     })
   }
 
   const getOrders = () => {
-    Axios.get('http://localhost:3001/orders').then((response) => {
-      console.log(  response);
+    Axios.get('http://localhost:3001/api/orders').then((response) => {
+      console.log(response);
       setOrderList(response.data);
     })
   }
   return (
     <div className="App">
+      <div class="title">
+      <label > SHOP NET by Kannan</label>
+      </div>
+      <p></p>
       <div className="information">
         <label>Name</label>
-        <input type="text" onChange={(event) => {setName(event.target.value)}}/>
+        <input type="text" onChange={(event) => { setName(event.target.value) }} />
         <label>Mailing Address</label>
-        <input type="text" onChange={(event) => {setAddress(event.target.value)}}/>
-      
-       
+        <input type="text" onChange={(event) => { setAddress(event.target.value) }} />
+
+
         <label>Product</label>
-        <input type="text" onChange={(event) => {setProduct(event.target.value)}}/>
+        <input type="text" onChange={(event) => { setProduct(event.target.value) }} />
         <label>Quantity</label>
-        <input type="number" onChange={(event) => {setQuantity(event.target.value)}}/>
+        <input type="number" onChange={(event) => { setQuantity(event.target.value) }} />
         <label>Price</label>
-        <input type="number" onChange={(event) => {setPrice(event.target.value)}}/>
+        <input type="number" onChange={(event) => { setPrice(event.target.value) }} />
         <button onClick={addOrder}>Add Order</button>
       </div>
 
       <div className="orders">
+        <p>
+          <button onClick={getOrders}>Show Orders</button> </p>
 
-      <button onClick={getOrders}>Show Orders</button>
-      {
-        orderList.map((val, key) => {
-          return <div className="order">
-            <h3>Name : {val.name}</h3>
-            <h3>Address : {val.address}</h3>
-            <h3>Product: {val.product}</h3>
-            <h3>Quantity: {val.quantity}</h3>
-            <h3>Price : {val.price}</h3>
-            </div>
-        })
-      }
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Product</th>
+            <th>Qty</th>
+            <th>setPrice</th>
+          </tr>
+          
+      
+        {
+          orderList.map((val, key) => {
+           
+            return  <tr>
+              <td>{val.name}</td>
+              <td>{val.address}</td>
+              <td>{val.product}</td>
+              <td>{val.quantity}</td>
+              <td>{val.price}</td>
+             </tr>
+          })
+        }
+          </table>
       </div>
     </div>
   );
---
+++ mysqldb/shopnet_ddl.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE shopnetorder;
+USE shopnetorder;
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `address` text NOT NULL,
+  `product` text NOT NULL,
+  `quantity` int NOT NULL,
+  `price` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT INTO `orders` VALUES (1,'Iyyappan','Keral','arun',108,0);
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-09-19 17:24:06
